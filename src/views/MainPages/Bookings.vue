c42fade253831961a103043ecb888ed10f68fb97
Abayomi Aluko <franicons@gmail.com>
2023-07-13 11:54:16 +0100

    feat: development

--- src/views/MainPages/Bookings.vue
+++ src/views/MainPages/Bookings.vue
@@ -4,15 +4,15 @@
 
     <template v-if="loading">
       <div class="grid grid-cols-3 gap-4">
-        <div class="" v-for="i in 3" :key="i">
-          <div class="border border-gray-200 shadow rounded-md p-5 mb-4" >
+        <div class="" v-for="i in 6" :key="i">
+          <div class="border border-gray-200 shadow rounded-md px-5 py-5 mb-4" >
             <div class="animate-pulse flex space-x-4">
-              <div class="flex-1 space-y-6 py-1">
-                <div class="space-y-4">
+              <div class="flex-1 py-1">
+                <div class="space-y-3">
                   <div class="grid grid-cols-4 gap-4">
-                    <div class="h-4 bg-gray-300 rounded col-span-2"></div>
+                    <div class="h-2 bg-gray-200 col-span-2"></div>
                   </div>
-                  <div class="h-4 bg-gray-300 rounded"></div>
+                  <div class="h-20 bg-gray-200"></div>
                 </div>
               </div>
             </div>
