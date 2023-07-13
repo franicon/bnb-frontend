48e97a42c10f544265057fc9b600653aa9129f68
Abayomi Aluko <franicons@gmail.com>
2023-07-13 16:20:23 +0100

    feat: development

---
+++ src/components/bookables/Aavailability.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1 class="text-sm uppercase font-bold text-gray-500">Check Availability</h1>
+    <div class="grid grid-cols-2 gap-4">
+      <div class="pt-1">
+        <label for="from" class="text-sm text-gray-400 font-semibold"> From </label>
+        <input placeholder="Start date" type="text" name="from" class="input w-full">
+      </div>
+      <div class="pt-1">
+        <label for="end" class="text-sm text-gray-400 font-semibold">To</label>
+        <input placeholder="End date" type="text" name="from" class="input w-full">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
