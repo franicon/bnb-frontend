b72a544acce9e8e49d9b3842a70f5ce365b2fd5b
Abayomi Aluko <franicons@gmail.com>
2023-07-13 02:27:28 +0100

    feat: development

--- src/views/MainPages/Bookings.vue
+++ src/views/MainPages/Bookings.vue
@@ -2,22 +2,17 @@
   <section class="max-w-5xl mx-auto">
     <h1 class="pb-2 font-semibold">Top City</h1>
     <span class=" grid grid-cols-3 gap-4 content-stretch">
-      <bookable-list-item
-          :key="bookable.id"
-          v-bind="bookable"
-          v-for="bookable in bookables"
-      />
+      <bookable-list-item :key="bookable.id" v-bind="bookable" v-for="bookable in bookables"/>
     </span>
   </section>
 </template>
 
 <script setup lang="ts">
-import {ref, onMounted} from "vue";
-
+import axios from "axios";
 const API = import.meta.env.VITE_API_URL
 
+import { ref, onMounted} from "vue";
 import BookableListItem from '@/components/bookables/BookableListItem.vue';
-import axios from "axios";
 
 const bookables = ref([
   {
